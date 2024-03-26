aeb56f3d62a7999996288bc8038a31b86516198a
RANGANATHAN08 <srn12666@gmail.com>
2024-03-26 17:03:37 +0530

    day 4

---
+++ task.js
@@ -0,0 +1,14 @@
+var request = new XMLHttpRequest();
+request.open("GET" ,"https://restcountries.com/v3.1/all")
+request.send();
+request.onload = function()
+{
+    var data = request.response;
+    var result = JSON.parse(data)
+   // console.log(result[0].name.common);
+   for(var i = 0 ; i < result.length ; i++)
+   {
+        console.log(result[i].name.common, result[i].region ,result[i].population , result[i].subregion);
+        console.log(result[i].flags.png)
+   }
+}
